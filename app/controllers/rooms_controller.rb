e01815931eaffe95d5179899073ee5537294c59b
Jeko0 <japulava@unisens.ge>
2022-08-17 14:16:32 +0400

    added messages for rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,14 +1,35 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_room
+  before_action :set_all_rooms 
+  before_action :set_all_users
 
   def index
-    @room = Room.new
-    @single_room = Room.find(params[:id])
-    @rooms = Room.public_rooms 
-    @users = User.all_except(current_user)
+    render "index"
   end
 
   def create 
     @room = Room.create(name: params["room"]["name"])
   end
+
+  def show 
+    @single_room = Room.find(params[:id])
+    @message = Message.new 
+    @messages = @single_room.messages.order(created_at: :asc)
+    render "index"
+  end
+
+  private
+
+    def set_room 
+      @room = Room.new
+    end
+
+    def set_all_rooms 
+      @rooms = Room.public_rooms 
+    end
+
+    def set_all_users
+      @users = User.all_except(current_user)
+    end
 end
